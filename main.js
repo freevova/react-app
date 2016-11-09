e8c5da3bfc9b0586094007ac01cb31be6d10e719
freevova <gula.volodya@gmail.com>
2016-11-09 22:36:10 +0200

    redux separete reducer

--- main.js
+++ main.js
@@ -14,17 +14,36 @@ const counter = (state = 0, action) => {
   }
 }
 
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        complited: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+      return {
+        ...state,
+        complited: !state.complited
+      }
+    default:
+      return state
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          complited: false
-        }
+        todo(undefined, action)
       ]
+    case 'TOGGLE_TODO':
+      return state.map((t) => todo(t, action))
     default:
       return state
   }
